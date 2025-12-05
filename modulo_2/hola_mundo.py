6da7312223cec94c92430b65ce0e532e1c3a38c0
Cristian Ramirez <cristianramirezborras@gmail.com>
2025-12-05 21:23:56 +0100

    Mi primer commit del Modulo 2

---
+++ modulo_2/hola_mundo.py
@@ -0,0 +1,6 @@
+print("¡Hola, mundo!") # Resultado impresion: ¡Hola, mundo!
+print("Cristian") # Resultado de la impresion: Cristian
+#print(Cristian) # La ejecucion da error por que detecta que el argumento no esta definido.
+#print "Cristian" # La ejecucion da error por que la funcion necesita envolver el argumento con parentesis.
+print('Cristian') # La ejecucion es correcta ya que python acepta las cadenas con comillas dobles o simples.
+
