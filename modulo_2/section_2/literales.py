8e3f5e8ec4ff4b002230a81750dd0930ca2289d3
Cristian Ramirez <cristianramirezborras@gmail.com>
2025-12-11 08:18:46 +0100

    reordenacion de archivos por modulos y secciones

---
+++ modulo_2/section_2/literales.py
@@ -0,0 +1,14 @@
+'''
+Los literales son tipos de datos cuyos valores estan determinados por el literal mismo
+'''
+# Este literal es una cadena de texto (string) (Las cadenas de texto deben tienen que ir envueltas en comillas dobles o comillas simples)
+print("Hola, soy Cristian")
+
+# Este literal es un Entero (int) 
+print(5)
+
+# Este literal es un Numero de punto flotante (float)
+print(5.5)
+
+# Este literal es un Boleano (bool)
+print(True)
