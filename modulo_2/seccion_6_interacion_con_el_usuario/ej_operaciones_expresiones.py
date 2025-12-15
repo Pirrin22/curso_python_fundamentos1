0d475f5d5a91f42c5b97add687bd4f5f99a677fc
Cristian Ramirez <cristianramirezborras@gmail.com>
2025-12-15 08:07:57 +0100

    Ejercicio de operedores y expresiones 1

---
+++ modulo_2/seccion_6_interacion_con_el_usuario/ej_operaciones_expresiones.py
@@ -0,0 +1,18 @@
+'''
+Ejercicio 2: OPERADORES Y EXPRESIONES
+
+Escenario
+Tu tarea es completar el código para poder evaluar la siguiente expresión:
+
+El resultado debe de ser asignado a y. Se cauteloso, observa los operadores y priorízalos. Utiliza cuantos paréntesis sean necesarios.
+
+Puedes utilizar variables adicionales para acortar la expresión (sin embargo, no es muy necesario). Prueba tu código cuidadosamente.
+'''
+
+x = float(input("Ingresa el valor para x: "))
+
+# Escribe tu código aquí.
+
+y = 1./(x + 1./(x + 1./(x + 1.)))
+
+print("y =", y)
