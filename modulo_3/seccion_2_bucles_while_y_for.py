2b015c87e6df9a6275ec886952babc8e483d4675
Cristian Ramirez <cristianramirezborras@gmail.com>
2025-12-22 08:49:07 +0100

    final de Ejercicios de bucles

--- modulo_3/seccion_2_bucles_while_y_for.py
+++ modulo_3/seccion_2_bucles_while_y_for.py
@@ -137,6 +137,55 @@ Tu tarea es escribir un programa que lea la cantidad de bloques que tienen los c
 Nota: La altura se mide por el número de capas completas - si los constructores no tienen la cantidad suficiente de bloques y no pueden completar la siguiente capa, terminan su trabajo inmediatamente.
 '''
 
+blocks = int(input("Ingresa el número de bloques: "))
+height = 0
+capa_actual = 1
+while blocks >= capa_actual:
+    blocks -= capa_actual
+    height += 1
+    capa_actual += 1
 
 
+print("la altura de la pirámide.", height)
 
+
+'''
+Ejercicio 6: LA HIPÓTESIS DE COLLATZ.
+
+Escenario
+En 1937, un matemático alemán llamado Lothar Collatz formuló una hipótesis intrigante (aún no se ha comprobado) que se puede describir de la siguiente manera:
+
+toma cualquier número entero que no sea negativo y que no sea cero y asígnale el nombre c0;
+si es par, evalúa un nuevo c0 como c0 ÷ 2;
+de lo contrario, si es impar, evalúe un nuevo  c0  como 3 × c0 + 1;
+si c0 ≠ 1, salta al punto 2.
+La hipótesis dice que, independientemente del valor inicial de c0, el valor siempre tiende a 1.
+
+Por supuesto, es una tarea extremadamente compleja usar una computadora para probar la hipótesis de cualquier número natural (incluso puede requerir inteligencia artificial), pero puede usar Python para verificar algunos números individuales. Tal vez incluso encuentres el que refutaría la hipótesis.
+
+Escribe un programa que lea un número natural y ejecute los pasos anteriores siempre que c0 sea diferente de 1. También queremos que cuente los pasos necesarios para lograr el objetivo. Tu código también debe mostrar todos los valores intermedios de c0.
+
+Sugerencia: la parte más importante del problema es como transformar la idea de Collatz en un bucle while- esta es la clave del éxito.
+'''
+
+user_number = int(input("Ingresa un numero entero: "))
+
+while user_number <= 0:
+    print(user_number)
+    print("El numero tiene que ser mayor que cero.")
+    user_number = int(input("Ingresa un numero entero: "))
+
+c0 = user_number
+steps = 0
+
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 = c0 // 2
+        print(c0)
+        steps += 1
+    else:
+        c0 = 3 * c0 + 1
+        steps += 1
+        print(c0)
+
+print('Pasos: ', steps)
