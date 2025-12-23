ee90f10d41ca95787d6619b81949c373e77e5697
Cristian Ramirez <cristianramirezborras@gmail.com>
2025-12-23 08:03:10 +0100

    Evaluacion seccion_2

---
+++ modulo_3/seccion_2_evaluacion_final_bucles.py
@@ -0,0 +1,35 @@
+'''
+EVALUACION FINAL SECCION 2:
+'''
+
+# Pregunta 1: Crea un bucle for que cuente de 0 a 10, e imprima números impares en la pantalla.
+for i in range(1, 11):
+    if i % 2 != 0:
+        print(i)
+
+
+# Pregunta 2: Crea un bucle while que cuente de 0 a 10, e imprima números impares en la pantalla.
+x = 1
+while x < 11:
+    if x % 2 != 0:
+        print(x)
+    x += 1
+    
+        
+# Pregunta 3: Crea un programa con un bucle for y una sentencia break. El programa debe iterar sobre los caracteres en una dirección de correo electrónico, salir del bucle cuando llegue al símbolo @ e imprimir la parte antes de @ en una línea.
+for ch in "john.smith@pythoninstitute.org":
+    if ch == "@":
+        break
+    print(ch, end='')
+print("\n")
+
+
+# Pregunta 4: Crea un programa con un bucle for y una sentencia continue. El programa debe iterar sobre una cadena de dígitos, reemplazar cada 0 con x, e imprimir la cadena modificada en la pantalla.
+for digit in "0165031806510":
+    if digit == "0":
+        print("x", end='')
+        continue
+    print(digit, end="")
+
+
+
