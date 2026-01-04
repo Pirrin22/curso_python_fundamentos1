a4996ce4d1862855fd36516c0f616b417e508d53
Cristian Ramirez <cristianramirezborras@gmail.com>
2026-01-04 09:04:48 +0100

    Ejercicio 2 de trabajndo con funciones.

--- modulo_4/seccion_3_devolucion_de_resultdos.py
+++ modulo_4/seccion_3_devolucion_de_resultdos.py
@@ -41,3 +41,51 @@ for i in range(len(test_data)):
         print("Fallido")
 
 
+
+'''
+Ejercicio 2: CUANTOS DIAS: ESCRIBIENDO Y USANDO TUS PROPIAS FUNCIONES.
+
+Tu tarea es escribir y probar una función que toma dos argumentos (un año y un mes) y devuelve el número de días del mes/año dado (mientras que solo febrero es sensible al valor year, tu función debería ser universal).
+
+La parte inicial de la función está lista. Ahora, haz que la función devuelva None si los argumentos no tienen sentido.
+
+Por supuesto, puedes (y debes) utilizar la función previamente escrita y probada (LAB 4.3.1.6). Puede ser muy útil. Te recomendamos que utilices una lista con los meses. Puedes crearla dentro de la función - este truco acortará significativamente el código.
+'''
+# Revisamos si el año es bisiesto o no.
+def is_year_leap(year):
+    if year % 4 != 0:
+        return False
+    elif year % 100 == 0 and year % 400 != 0:
+        return False
+    elif year % 4 == 0 and year % 100 != 0:
+        return True
+    else:
+        return True
+
+def days_in_month(year, month):
+    dias_por_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if year < 1582:
+        return None
+    elif month < 1 or month > 12:
+        return None
+    else:
+        if month == 2 and is_year_leap(year):
+            return 29
+        return dias_por_mes[month - 1]
+    
+    
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+    yr = test_years[i]
+    mo = test_months[i]
+    print(yr, mo, "->", end="")
+    result = days_in_month(yr, mo)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Fallido")
+
+
